b0c4225a32f8d1db4db07147fa4f829a693422da
goknurginer <goknurginer@gmail.com>
2018-07-18 14:11:41 +1000

    initial files are added.

---
+++ scripts/alignment.R
@@ -0,0 +1,28 @@
+setwd("./fastq")
+date <- Sys.Date(); sink(paste0(date, "-alignment.R.log"), split = TRUE)
+
+library(Rsubread)
+
+files <- dir(pattern=".fastq.gz")
+# paired-end
+# files1 <- dir(pattern = "fastq.gz")[seq(1, length(f), by = 2)]
+# files2 <- dir(pattern = "fastq.gz")[seq(2, 28, by = 2)] # this line is for paired-end alignment
+
+align(index = "/usr/local/bioinfsoftware/subread/subread_index/hg19", readfile = files, output_file = "../bam/", nthreads = 5, unique = TRUE)
+
+setwd("../bam")
+
+files <- dir(pattern = "fastq.gz.subread.bam")
+
+BAM <- files[!grepl(".indel", files)]
+counts <- featureCounts(BAM, annot.inbuilt = "hg19")
+
+propmap <- propmapped(BAM)
+ann <- getInBuiltAnnotation("hg19")
+
+d <- list(count = counts, propmap = propmap, ann = ann)
+save(d, file = paste0("../RData", date, "yoruba-rsubread-hg19-gene.RData"))
+
+print(sessionInfo())
+
+sink()
